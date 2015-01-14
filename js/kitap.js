f020127258fc2567db5cf26ba0c43a6e3e163c2c
ALİ CAN UÇAR <alicanucar94@gmail.com>
2015-01-14 21:15:47 +0200

    hatalar düzeltildi

---
+++ js/kitap.js
@@ -0,0 +1,163 @@
+kitap_katalog([{
+    "KitapAdi": " Kafamda Bir Tuhaflık ",
+    "Yazar": "Orhan Pamuk",
+    "Yayinevi": "YKY Yayınları",
+    "BasimYili": "2014",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/kafamda-bir-tuhaflik-orhan-pamuk/tanim.asp?sid=L2W8MWF8KB4B9QBEJXQ4"
+}, {
+    "KitapAdi": "Abim Deniz ",
+    "Yazar": "Can Dündar ",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "2014",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/abim-deniz-can-dundar/tanim.asp?sid=KQF3HW1TID5DRSWDUBOV"
+}, {
+    "KitapAdi": "Kürk Mantolu Madonna ",
+    "Yazar": "Sabahattin Ali",
+    "Yayinevi": "YKY Yayınları",
+    "BasimYili": "1943",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/kurk-mantolu-madonna-sabahattin-ali/tanim.asp?sid=PFWHROPSSK3OXPUW6J1X"
+}, {
+    "KitapAdi": "1984",
+    "Yazar": "George Orwell",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "1949",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/1984-george-orwell/tanim.asp?sid=PRC4H97IEB4ZIQCPZN0F"
+}, {
+    "KitapAdi": "Tutunamayanlar ",
+    "Yazar": "Oğuz Atay",
+    "Yayinevi": "İletişim Yayınları",
+    "BasimYili": "1972",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/tutunamayanlar-butun-eserleri-1-oguz-atay/tanim.asp?sid=Y8L3AR75WT6AJHB068L4"
+}, {
+    "KitapAdi": "Sineklerin Tanrısı",
+    "Yazar": "William Golding ",
+    "Yayinevi": "İş Bankası Yayınları",
+    "BasimYili": "1954",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/sineklerin-tanrisi-william-golding/tanim.asp?sid=OFCCPQ43I2PG7DJETKHL"
+}, {
+    "KitapAdi": "Bülbülü Öldürmek",
+    "Yazar": "Harper Lee",
+    "Yayinevi": "Sel Yayıncılık",
+    "BasimYili": "1960",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/bulbulu-oldurmek-harper-lee/tanim.asp?sid=EYLJ272XB2YYTA46INKQ"
+}, {
+    "KitapAdi": "Yüzyıllık Yalnızlık",
+    "Yazar": "Gabriel García Márquez",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "1967",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/yuzyillik-yalnizlik-gabriel-garcia-marquez/tanim.asp?sid=UU8YLLPRLY4KJ2NXPUD1"
+}, {
+    "KitapAdi": "Uçurtma Avcısı",
+    "Yazar": "Halit Hüseyni",
+    "Yayinevi": "Everest Yayınları",
+    "BasimYili": "2003",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/ucurtma-avcisi-khaled-hosseini/tanim.asp?sid=C6DFXJ0J5A3AK825VIMZ"
+}, {
+    "KitapAdi": "Tehlikeli Oyunlar",
+    "Yazar": "Oğuz Atay",
+    "Yayinevi": "İletişim Yayınları",
+    "BasimYili": "1973",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/tehlikeli-oyunlar-oguz-atay/tanim.asp?sid=AKP274SCMT7I2SPN9VQI"
+}, {
+    "KitapAdi": "Kırmızı Pazartesi",
+    "Yazar": "Gabriel García Márquez",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "1981",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/kirmizi-pazartesi-gabriel-garcia-marquez/tanim.asp?sid=BA5IMUSPRK2AOFNBQJKH"
+}, {
+    "KitapAdi": "Genç Werther'in Acıları",
+    "Yazar": "Johann Wolfgang von Goethe",
+    "Yayinevi": "Karakteristik Yayınları",
+    "BasimYili": "1774",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/genc-wertherin-acilari-goethe/tanim.asp?sid=B44KVCMCRW7WQBN0T75U"
+}, {
+    "KitapAdi": "Huzur",
+    "Yazar": "Ahmet Hamdi Tanpınar",
+    "Yayinevi": "Dergah Yayınları",
+    "BasimYili": "1949",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/huzur-ahmet-hamdi-tanpinar/tanim.asp?sid=D47R060I0H1RXBDCQ4BF"
+}, {
+    "KitapAdi": "İnce Memed",
+    "Yazar": "Yaşar Kemal",
+    "Yayinevi": "YKY Yayınları",
+    "BasimYili": "1955",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/ince-memed-1-yasar-kemal/tanim.asp?sid=Q8OWVHD4T7NN0KMB18OG"
+}, {
+    "KitapAdi": "Sırça Köşk",
+    "Yazar": "Sabahattin Ali",
+    "Yayinevi": "YKY Yyınları",
+    "BasimYili": "1947",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/kafamda-bir-tuhaflik-orhan-pamuk/tanim.asp?sid=L2W8MWF8KB4B9QBEJXQ4"
+}, {
+    "KitapAdi": "Suç ve Ceza",
+    "Yazar": "Fyodor Dostoyevski",
+    "Yayinevi": "Kaldırım Yayınları",
+    "BasimYili": "1866",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/suc-ve-ceza-fyodor-mihailovic-dostoyevski/tanim.asp?sid=IN11MVD04C2C3N59QMS0"
+}, {
+    "KitapAdi": "Nietzsche Ağladığında",
+    "Yazar": "Irvin D. Yalom",
+    "Yayinevi": "Ayrıntı Yayınları",
+    "BasimYili": "1992",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/nietzsche-agladiginda-irvin-d-yalom/tanim.asp?sid=BPJZ8X5XON7EM989VVFG"
+}, {
+    "KitapAdi": "Senden Önce Ben",
+    "Yazar": "Jojo Moyes",
+    "Yayinevi": "Pegasus Yayınları",
+    "BasimYili": "2012",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/senden-once-ben-jojo-moyes/tanim.asp?sid=KGXH5A5RA1EY9LFVE577"
+}, {
+    "KitapAdi": "Simyacı",
+    "Yazar": "Paulo Coelho",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "1988",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/simyaci-paulo-coelho/tanim.asp?sid=FT47D2ZBOY0Y5G2ZRL29"
+}, {
+    "KitapAdi": "Sofie'nin Dünyası",
+    "Yazar": "Jostein Gaarder",
+    "Yayinevi": "Plan Yıyıncılık",
+    "BasimYili": "1991",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/sofienin-dunyasi-jostein-gaarder/tanim.asp?sid=ITEHH2F2OS0DN4ROWC1L"
+}, {
+    "KitapAdi": "Fareler ve İnsanlar",
+    "Yazar": "John Steinbeck",
+    "Yayinevi": "Sel Yayınları",
+    "BasimYili": "1937",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/fareler-ve-insanlar-john-steinbeck/tanim.asp?sid=BUUMIK01H704XP1DF86Z"
+}, {
+    "KitapAdi": "Saatleri Ayarlama Enstitüsü",
+    "Yazar": "Ahmet Hamdi Tanpınar",
+    "Yayinevi": "Dergah Yayınları",
+    "BasimYili": "2013",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/saatleri-ayarlama-enstitusu-ahmet-hamdi-tanpinar/tanim.asp?sid=E75JVWM6MS2AQDXZ2WF1"
+}, {
+    "KitapAdi": "Yeraltından Notlar",
+    "Yazar": "Fyodor Dostoyevski",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "1864",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/yeraltindan-notlar-fyodor-mihailovic-dostoyevski/tanim.asp?sid=PLKQ5KXT840Z9EO3T8NE"
+}, {
+    "KitapAdi": "Olasılıksız",
+    "Yazar": "Adam Fawer",
+    "Yayinevi": "April Yayıncılık",
+    "BasimYili": "2005",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/olasiliksiz-adam-fawer/tanim.asp?sid=RWII85Q3Y21HSZU1XH0B"
+}, {
+    "KitapAdi": "Kafamda Bir Tuhaflık",
+    "Yazar": "Orhan Pamuk",
+    "Yayinevi": "YKY Yayınları",
+    "BasimYili": "2012",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/kafamda-bir-tuhaflik-orhan-pamuk/tanim.asp?sid=L2W8MWF8KB4B9QBEJXQ4"
+}, {
+    "KitapAdi": "Deliduman",
+    "Yazar": "Emrah Serbes",
+    "Yayinevi": "İletişim Yayıncılık",
+    "BasimYili": "2014",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/deliduman-emrah-serbes/tanim.asp?sid=UXUAZ3EMW53HMMRZCBSY"
+}, {
+    "KitapAdi": "Yabancı",
+    "Yazar": "Albert Camus",
+    "Yayinevi": "Can Yayınları",
+    "BasimYili": "1942",
+    "SatinAlmaLinki": "http://www.idefix.com/kitap/yabanci-albert-camus/tanim.asp?sid=F5R8GROXHY4CNFIFTD63"
+} ]);
---
+++ js/tez.js
@@ -0,0 +1,96 @@
+tez_katalog([{
+    "baslik": "Bilgisayar kontrollü tektel sistemi ile blok kesmede etken faktörlerin incelenmesi ",
+    "Yazar": "Emre Yılmazkaya ",
+    "YayinYili": "2013",
+    "tur": "Doktora Tezi"
+}, {
+    "baslik": "Bilgisayar oyunları, yeni medya ve kimlik: kuramsal arayışlar ",
+    "Yazar": "Umut Yener Kaya ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Üniversite öğrencilerinin denetim odağı ve bilgisayar laboratuvarına yönelik tutumlarının siberaylak ",
+    "Yazar": "Sevil Yaşar ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Kurumsal belge ve arşiv yönetimi uygulamaları: kalkınma bakanlığı örneği ",
+    "Yazar": "Gülçin Özbay ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Türkiye'nin bilim-teknoloji politikalarında kütüphane kurumuna yaklaşım ",
+    "Yazar": "Bülent Yılmaz ",
+    "YayinYili": "2012 ",
+    "tur": "Makale "
+}, {
+    "baslik": " Türkiye'deki kırgız ve kazak kökenli göçmen girişimciler üzerine bir alan araştırması. ",
+    "Yazar": "Aisuluu Subanova ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Ankara'daki anaokullarının okuma alışkanlığına hazırlık yeterlilikleri açısından değerlendirilmesi ",
+    "Yazar": "Selda Ekici ",
+    "YayinYili": "2014 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Neo-kemalist bir haraket:cumhuriyet mitingleri ",
+    "Yazar": "Gökçe Zeybek Kabakcı ",
+    "YayinYili": "2011 ",
+    "tur": "Makale "
+}, {
+    "baslik": " Doğa, sanat ve biyomimetik bilim ",
+    "Yazar": "Melda Genç ",
+    "YayinYili": "2013 ",
+    "tur": "Sanatta Yeterlilik Tezi "
+}, {
+    "baslik": " E-devlet kapsamında kurumsal bilgi sistemlerinin değerlendirilmesi: içişleri bakanlığı örneği ",
+    "Yazar": "Şahika Eroğlu ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Nüfuz, iktidar, servet ve lüks tüketim: anadolu selçuklularında siyasal nüfuz, güç ve otorite ilişkisi ",
+    "Yazar": "Necmettin Durmuş ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Sosyo kültürel değişim kapsamında iç mekanda renk kullanımı ve analizi ",
+    "Yazar": "Esmer Ahundzade ",
+    "YayinYili": "2014 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Kappadokia göreme vadisinde meryem siklusu ",
+    "Yazar": "Fatma Ayten Alev ",
+    "YayinYili": "2014 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Amputelerde futbolun performans üzerine etkilerinin belirlenmesi ",
+    "Yazar": "Zehra Güçhan ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Diz osteoartritli hastalarda izokinetik ve aerobik egzersizin serum interlökin-6 ve tümör nekrozis faktör- alfa düzeylerine, ağrı ve fonksiyonel aktivite üzerine etkisi ",
+    "Yazar": "Gülbüz Samut ",
+    "YayinYili": "2013 ",
+    "tur": "Tıpta Uzmanlık Tezi "
+}, {
+    "baslik": "Duygusal olayların hatırlanması: bir göz izleme çalışması ",
+    "Yazar": "Çiğdem Gülçay ",
+    "YayinYili": "2014 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Cumhuriyet dönemi edebiyat ve sanat dergilerinde şiir kuramı ve şiir eleştirisi:1950-1970 ",
+    "Yazar": "Hande Karapınar ",
+    "YayinYili": "2013 ",
+    "tur": "Yüksek Lisans Tezi "
+}, {
+    "baslik": "Türk-yunan ilişkilerinde nato'nun tutumu ve bunun türkiye-nato ilişkilerine yansımaları ",
+    "Yazar": "Dinçer Bayer",
+    "YayinYili": "2013 ",
+    "tur": "Doktora Tezi "
+}, {
+    "baslik": "Sosyal çevrenin etkilerinin çocukların suç ve problemli davranışları ile ilişkileri ",
+    "Yazar": "Sevgi Çoban ",
+    "YayinYili": "2012 ",
+    "tur": "Doktora Tezi "
+}]);
